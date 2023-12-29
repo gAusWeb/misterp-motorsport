c2e44f301237e876203aa7b210dd9eb65be21a93
Gavin McQuienn <gavinmcquienn@aceik.com.au>
2023-12-29 12:08:48 +1100

    added md file find test #1. commented-out reference to next-mdx-remote.

--- app/test-route/[slug]/page.tsx
+++ app/test-route/[slug]/page.tsx
@@ -1,3 +1,23 @@
+import fs from "fs";
+import matter from "gray-matter";
+
 export default function Page({ params }: { params: { slug: string } }) {
+  const posts = fs.readdirSync("./app/blogs").map((filename) => {
+    if (filename.split(".").pop() !== "md" && filename !== undefined) return;
+    const slug = filename.replace(".md", "");
+    const markdownWithMetadata = fs
+      .readFileSync(`./app/blogs/${filename}`)
+      .toString();
+    const { data, content } = matter(markdownWithMetadata);
+    const frontmatter = {
+      ...data,
+      slug,
+    };
+    return {
+      slug,
+      frontmatter,
+      content,
+    };
+  });
   return <div>My Post: {params.slug}</div>;
 }
